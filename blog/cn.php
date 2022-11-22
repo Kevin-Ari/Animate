18f31a9067fbb1dbc1af7715805a09f721a390ba
Galo98 <galonahuel98@gmail.com>
2022-11-22 10:16:56 -0300

    Animate mehn

--- blog/cn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conexion = mysqli_connect('localhost', 'root', '', 'bd_prueba');
-//if (!$conexion) {
-//    echo "Error al conectar a la base de datos";
-//}
-//else {
-//    echo "Conectado a la base de datos";
-//}
